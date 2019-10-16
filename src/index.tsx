e26337771a201702bcf4120a4cdc03ea290f2a58
Jakub Pawłot <kjubon@gmail.com>
2019-10-16 21:39:29 +0200

    Add tslint, move some deps to devDependencies

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.scss';
 import App from './components/App/App';
+import './index.scss';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
